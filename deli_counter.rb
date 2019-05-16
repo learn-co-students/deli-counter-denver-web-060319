7e6f65e501d61591d6acae0db19e722766895bc2
“Patrick <pctennislessons@gmail.com>
2019-05-15 21:40:47 -0600

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def line(deli_line)
+    line_string = "The line is currently:"
+    if deli_line.count == 0
+        puts "The line is currently empty." 
+    else deli_line.each_with_index do |key, index| 
+        line_string =  line_string + " #{index+1}. #{key}"
+        end 
+        puts line_string
+    end
+end
+
+def take_a_number(deli_line, customer)
+    deli_line << customer
+    puts "Welcome, #{customer}. You are number #{deli_line.count} in line."
+end
+
+def now_serving(deli_line)
+    puts deli_line.count == 0 ? "There is nobody waiting to be served!" : "Currently serving #{deli_line.shift}."
+end
